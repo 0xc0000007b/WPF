9f4ac1f82fd37ed1a5ff1867f817f2ab0dfa3b33
0xc000007b <rammleid123@gmail.com>
2023-01-11 19:20:37 +0300

    move-startup-window-to-views

--- WpfApp4/View/Windows/MainWindow.xaml.cs
+++ WpfApp4/View/Windows/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace WpfApp4
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
