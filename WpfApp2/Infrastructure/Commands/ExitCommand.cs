293e085242636a2c79ae1b9545b2b319c4af3128
0xc000007b <rammleid123@gmail.com>
2023-01-13 18:43:40 +0300

    add-business-logic

---
+++ WpfApp2/Infrastructure/Commands/ExitCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApp2.Infrastructure.Commands.Commands;
+
+public class ExitCommand : Command
+{
+    public ExitCommand(Action<object> Execute, Func<object, bool> CanExecute = null) : base(Execute, CanExecute)
+    {
+        Application.Current.Shutdown();
+    }
+}
