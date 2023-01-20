da3067f012787342f09c845b34d962cd279baf5b
0xc000007b <rammleid123@gmail.com>
2023-01-20 15:08:47 +0300

    add-filtering

--- WpfApp2/Views/Windows/MainWindow.xaml.cs
+++ WpfApp2/Views/Windows/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp2.Models;
 
 namespace WpfApp2
 {
@@ -23,6 +26,24 @@ namespace WpfApp2
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GroupCollection_OnFilter(object sender, FilterEventArgs e)
+        {
+            if(!(e.Item is Group g)) return;
+            if(g.Name is null) return;
+            var filterText = FilterBox.Text;
+            if (filterText.Length == 0) return;
+            if(g.Name.Contains(filterText,StringComparison.OrdinalIgnoreCase))return;
+            if(g.Description != null && g.Description.Contains(filterText, StringComparison.OrdinalIgnoreCase)) return;
+            e.Accepted = false;
+        }
+
+        private void OnGroupFilterTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            var collection = (CollectionViewSource)textBox.FindResource("GroupsCollection");
+            collection.View.Refresh();
         }
     }
 }
