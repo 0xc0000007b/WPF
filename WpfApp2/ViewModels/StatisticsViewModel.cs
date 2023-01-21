aeff7e72e9cc4c51c92986f881cbbaab1ce4858c
0xc000007b <rammleid123@gmail.com>
2023-01-21 13:49:02 +0300

    teach app how to get data from server

--- WpfApp2/ViewModels/StatisticsViewModel.cs
+++ WpfApp2/ViewModels/StatisticsViewModel.cs
@@ -1,4 +1,9 @@
-﻿using WpfApp2.Services;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using WpfApp2.Infrastructure.Commands;
+using WpfApp2.Models;
+using WpfApp2.Services;
 using WpfApp2.ViewModels.Base;
 
 namespace WpfApp2.ViewModels;
@@ -9,9 +14,32 @@ public class StatisticsViewModel : ViewModelBase
     private DataService _dataService;
     private  MainWindowViewModel _mainWindowViewModel { get; }
 
+    #region Commands
+
+    public ICommand RefreshStatistics { get; }
+
+    private void OnRefreshStatistics(object e)
+    {
+        Countries = _dataService.GetData();
+    }
+
+    private bool CanRefreshStatistics(object e) => true;
+
+    #endregion
+
+    private IEnumerable<Country> _countries;
+
+    public IEnumerable<Country> Countries
+    {
+        get => _countries;
+
+        private set => SetField(ref _countries, value);
+    }
+    
     public StatisticsViewModel(MainWindowViewModel mainWindowViewModel)
     {
         _mainWindowViewModel = mainWindowViewModel;
         _dataService = new DataService();
+        RefreshStatistics = new Command(OnRefreshStatistics, CanRefreshStatistics);
     }
 }
