11f682250007c59f4deaa4c1a1fe200e817eb923
0xc000007b <rammleid123@gmail.com>
2023-01-21 23:29:38 +0300

    добавил несколько конверторов

---
+++ WpfApp2/Infrastructure/Converters/CompositeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfApp2.Infrastructure.Converters;
+
+public class CompositeConverter : Converter
+{
+
+    public IValueConverter First { get; set; }
+    public IValueConverter Second { get; set; }
+
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var firstRes = First?.Convert(value, targetType, parameter, culture) ?? value;
+        var secondRes = First?.Convert(firstRes, targetType, parameter, culture) ?? firstRes;
+
+        return secondRes;
+    }
+
+    public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var secondBackRes = Second?.ConvertBack(value, targetType, parameter, culture) ?? value;
+        var firstBackRes = First?.ConvertBack(secondBackRes, targetType, parameter, culture) ?? secondBackRes;
+        return firstBackRes;
+    }
+}
---
+++ WpfApp2/Infrastructure/Converters/Converter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfApp2.Infrastructure.Converters;
+
+
+public abstract class Converter : IValueConverter
+{
+    public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+    public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotSupportedException("Not supported");
+    
+}
