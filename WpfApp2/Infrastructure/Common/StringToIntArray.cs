92048b0f84840cf379f68f600626e164ccb32bfc
0xc000007b <rammleid123@gmail.com>
2023-01-22 12:29:18 +0300

    проверка конвертеров

---
+++ WpfApp2/Infrastructure/Common/StringToIntArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Markup;
+
+namespace WpfApp2.Infrastructure.Common;
+
+public class StringToIntArray : MarkupExtension
+{
+    [ConstructorArgument("Str")]
+    public string Str { get; set; }
+
+    public StringToIntArray()
+    {
+        
+    }
+
+    public StringToIntArray(string str) => this.Str = str;
+
+    public override object ProvideValue(IServiceProvider serviceProvider) =>
+        Str.Split(new []{Sep}, StringSplitOptions.RemoveEmptyEntries)
+            .DefaultIfEmpty()
+            .Select(int.Parse)
+            .ToArray();
+    
+
+    public char Sep { get; set; } = ';';
+}
